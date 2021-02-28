ba50f3574e5a732c84f4a0bc056f0938b1e1119e
Yi Lu <yilu1021@gmail.com>
2021-02-28 11:29:24 -0500

    initial commit

---
+++ cmake-build-debug/CMakeFiles/login_registration_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/login_registration_system.dir/main.cpp.o"
+  "login_registration_system"
+  "login_registration_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/login_registration_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <string>
+
+int main() {
+    std::cout << "#1 Register" << std::endl;
+    std::cout << "#2 Log In" << std::endl;
+    std::string input_choice_str;
+    std::cin >> input_choice_str;
+    std::cout << input_choice_str;
+
+//    int input_choice_int;
+//    stringstream
+//    std::cin
+    return 0;
+}
